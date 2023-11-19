7e2abb9a0ca26cdf9a95cc2dcd255a412eed4c3e
Mathilde Arconte <signup@marconte.fr>
2023-11-20 00:45:12 +0100

    🐛 fix(css): update transform property in app.css 🐛 fix(html): fix background color in top-bar-boxed in index.html 🐛 fix(js): add/remove class to map-box element in animate-input.js ✨ feat(html): add report-box project cards in index.html

--- dist/js/animate-input.js
+++ dist/js/animate-input.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded",function () {
-	var topBar = document.getElementById('top-bar__input');
-	var heroSection = document.getElementById('hero-section');
-
+	const topBar = document.getElementById('top-bar__input');
+	const heroSection = document.getElementById('hero-section');
+const map = document.getElementById('map-box');
 	window.onscroll = function () {
 		// Get the bottom position of the hero section
-		var heroBottom = heroSection.offsetTop + heroSection.offsetHeight;
+		const heroBottom = heroSection.offsetTop + heroSection.offsetHeight - 150;
 
 		// Check if the page is scrolled past the hero section
 		if (window.pageYOffset > heroBottom) {
 			topBar.style.display = 'flex'; // Show the top bar
+			map.classList.add('scrolled');
 		} else {
 			topBar.style.display = 'none'; // Hide the top bar
+			map.classList.remove('scrolled');
 		}
 	};
 });
