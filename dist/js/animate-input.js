d743177a506e6d8e5999a89d83b259fcaacb67b6
Mathilde Arconte <signup@marconte.fr>
2023-11-20 01:28:14 +0100

    Responsive(WIP)

--- dist/js/animate-input.js
+++ dist/js/animate-input.js
@@ -1,18 +1,22 @@
 document.addEventListener("DOMContentLoaded",function () {
 	const topBar = document.getElementById('top-bar__input');
 	const heroSection = document.getElementById('hero-section');
-const map = document.getElementById('map-box');
-	window.onscroll = function () {
-		// Get the bottom position of the hero section
-		const heroBottom = heroSection.offsetTop + heroSection.offsetHeight - 150;
+	const map = document.getElementById('map-box');
+	// if not on mobile device
+	if (window.innerWidth > 768) {
+		window.onscroll = function () {
+			// Get the bottom position of the hero section
+			const heroBottom = heroSection.offsetTop + heroSection.offsetHeight - 150;
 
-		// Check if the page is scrolled past the hero section
-		if (window.pageYOffset > heroBottom) {
-			topBar.style.display = 'flex'; // Show the top bar
-			map.classList.add('scrolled');
-		} else {
-			topBar.style.display = 'none'; // Hide the top bar
-			map.classList.remove('scrolled');
-		}
-	};
+			// Check if the page is scrolled past the hero section
+			if (window.pageYOffset > heroBottom) {
+				topBar.style.display = 'flex'; // Show the top bar
+				map.classList.add('scrolled');
+			} else {
+				topBar.style.display = 'none'; // Hide the top bar
+				map.classList.remove('scrolled');
+			}
+		};
+	}
+
 });
